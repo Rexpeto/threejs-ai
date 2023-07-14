9e9e63078ee5e40480241a3b2e4a165f9495a4c5
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-14 15:44:13 -0400

    Add news imports component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,8 @@
 import CustomButton from "./CustomButton";
 import ThemeSwitch from "./ThemeSwitch";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
 
-export { CustomButton, ThemeSwitch };
+export { CustomButton, ThemeSwitch, AIPicker, ColorPicker, FilePicker, Tab };
