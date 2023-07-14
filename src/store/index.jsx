50346c0a61d263fd0cc0421533ff40aebfc89605
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-14 17:20:12 -0400

    Use logo Carlos Gallardo

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -5,8 +5,8 @@ const state = proxy({
     color: "#EFBD48",
     isLogoTexture: true,
     isFullTexture: false,
-    logoDecal: "./threejs.png",
-    fullDecal: "./threejs.png"
+    logoDecal: "./LogoShirt.png",
+    fullDecal: "./LogoShirt.png"
 });
 
 export default state;
