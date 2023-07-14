802f3209fb901e8a74ecf2dd4d89899de63ff8d4
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-13 20:36:04 -0400

    Support dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{jsx, js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: "class",
+    content: ["./index.html", "./src/**/*.{jsx, js}"],
+    theme: {
+        extend: {}
+    },
+    plugins: []
 };
